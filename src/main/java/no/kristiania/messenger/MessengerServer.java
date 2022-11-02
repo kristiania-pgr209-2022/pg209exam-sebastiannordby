4448dabf72e71bcfb9765a8bb86ffc7f26b4e3b7
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-11-02 11:29:29 +0100

    MessengerServer: Added jetty-webapp

--- src/main/java/no/kristiania/messenger/MessengerServer.java
+++ src/main/java/no/kristiania/messenger/MessengerServer.java
@@ -1,12 +1,20 @@
 package no.kristiania.messenger;
 
+import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MessengerServer {
     private static final Logger logger = LoggerFactory.getLogger(MessengerServer.class);
+    private final Server server;
 
-    public MessengerServer() {
+    public MessengerServer(int port) {
+        this.server = new Server(port);
+    }
 
+    public void start() throws Exception {
+        logger.info("Starting server..");
+        server.start();
+        logger.info("Server started at: ", server.getURI());
     }
 }
