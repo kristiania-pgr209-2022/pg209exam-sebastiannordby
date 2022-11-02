e41c203a47348f0842282f1b3dff4bfd8fa36387
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-11-02 13:23:46 +0100

    Styling react app to look like messenger

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -2,14 +2,122 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let selectedUser = null;
 
-  return (
-    <div className="App">
-      <h1>Messenger - Kristiania</h1>
-    </div>
-  )
+const USERS = [
+    {
+        id: 1,
+        username: "Cyllon",
+        name: "Sebastian Nordby"
+    },
+    {
+        id: 2,
+        username: "ridens",
+        name: "Mats"
+    },
+    {
+        id: 3,
+        username: "alladin",
+        name: "Alladin Muhammed"
+    }
+];
+
+const PANEL_DATA = [
+    {
+        username: "Alladin",
+        type: "user",
+        displayName: "Alladin"
+    },
+    {
+        username: "Mohammed",
+        type: "user",
+        displayName: "Ola"
+    },
+    {
+        username: "Ola",
+        type: "user",
+        displayName: "Ola"
+    }
+];
+
+function UserSelector({onUserSelected}) {
+    return (
+        <div className={"user-selector"}>
+            {USERS.map(x =>
+                <div className={"user-card"} key={x.id} onClick={() => onUserSelected(x)}>
+                    <label>{x.username}</label>
+                    <p>{x.name}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+function MainView() {
+    const [panelData, setPanelData] = useState();
+
+    return (
+        <div className={"main-view"}>
+            <header>
+                <nav>
+                    <h2>Kristiania Messenger</h2>
+                </nav>
+            </header>
+            <div className={"content"}>
+                <div class={"selector"}>
+                    <PanelView setPanelData={setPanelData}></PanelView>
+                </div>
+                <div class={"messages"}>
+                    <MessageView source={panelData}></MessageView>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function MessageView({ source }) {
+    return (
+        <div class="message-view">
+            <h3 className={"secondary-title"}>Meldinger</h3>
+        </div>
+    );
+}
+
+function PanelView({setPanelData}) {
+    return (
+        <div className={"panel-view"}>
+            <div>
+                <h3 className={"secondary-title"}>Personer</h3>
+            </div>
+            <div>
+                {PANEL_DATA.forEach(x =>
+                    <div key={x.displayName} onClick={() => setPanelData(x)}>
+                        <span>{x.displayName}</span>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+function App() {
+    const [user, setUser] = useState();
+
+    function onUserSelected(user) {
+        setUser(user);
+    }
+
+    if(user == null) {
+        return (
+            <div className={"welcome-screen"}>
+                <h1>Kristiania Messenger</h1>
+                <h2>Velg bruker for å fortsette</h2>
+                <UserSelector onUserSelected={onUserSelected}></UserSelector>
+            </div>
+        );
+    } else {
+        return (<MainView/>);
+    }
 }
 
 export default App
