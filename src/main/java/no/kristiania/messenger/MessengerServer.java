64f7ceaf72a75045743b191cce7bfcd972785837
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-11-02 11:26:26 +0100

    Added logging to application with SLF4j

--- src/main/java/no/kristiania/messenger/MessengerServer.java
+++ src/main/java/no/kristiania/messenger/MessengerServer.java
@@ -1,4 +1,12 @@
 package no.kristiania.messenger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MessengerServer {
+    private static final Logger logger = LoggerFactory.getLogger(MessengerServer.class);
+
+    public MessengerServer() {
+
+    }
 }
