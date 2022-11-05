24c7ef860708ba504463e0c0aff3365147f3b941
matsjohaa <mmaatts@gmail.com>
2022-11-05 11:04:36 +0100

    updated MessageDao interface

--- src/main/java/no/kristiania/messenger/dao/MessageDao.java
+++ src/main/java/no/kristiania/messenger/dao/MessageDao.java
@@ -2,6 +2,10 @@ package no.kristiania.messenger.dao;
 
 import no.kristiania.messenger.entities.User;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface MessageDao {
     int sendNewMessage (User entity) throws Exception;
+    List findMessageBetween(User sender, User receiver) throws SQLException;
 }
