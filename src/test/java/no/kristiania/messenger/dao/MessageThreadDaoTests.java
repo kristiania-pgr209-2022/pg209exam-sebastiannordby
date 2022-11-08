5d5782630319458ee2d9c11017fe7c0ef7626c72
matsjohaa <mmaatts@gmail.com>
2022-11-08 14:07:04 +0100

    updated test

--- src/test/java/no/kristiania/messenger/dao/MessageThreadDaoTests.java
+++ src/test/java/no/kristiania/messenger/dao/MessageThreadDaoTests.java
@@ -73,7 +73,7 @@ public class MessageThreadDaoTests {
 
         assertThat(messageThreads).isNotNull();
         assertThat(messageThreads).isNotEmpty();
-        //assertThat(messageThreads).isEqualTo(messageThread);
+        //assertThat(messageThreads).contains(messageThread);
 
     }
 
