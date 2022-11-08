8f31406f0bb677b59ca736ad931b97a6e0308fd7
matsjohaa <mmaatts@gmail.com>
2022-11-08 20:36:30 +0100

    Fixed integration

--- src/main/resources/db/migration/V004__add_message_read.sql
+++ src/main/resources/db/migration/V004__add_message_read.sql
@@ -2,8 +2,7 @@ CREATE TABLE MessageRead(
     Id INT IDENTITY(1, 1),
     UserId int not null ,
     MessageId int not null ,
-    ReadAt datetime
-
+    ReadAt datetime,
     CONSTRAINT UserId_FK FOREIGN KEY(UserId) REFERENCES Users(Id),
     CONSTRAINT MessageId_FK FOREIGN KEY(MessageId) REFERENCES Messages(Id)
 );
